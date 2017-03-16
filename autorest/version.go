e09a1841b5e6a5a1bd86c05974763d2c8c221767
Cosmin Cojocar <cosmc@microsoft.com>
2017-03-16 07:43:27 +0100

    Fix unhandled error warning generated by gas static code analyzer

--- autorest/version.go
+++ autorest/version.go
@@ -18,11 +18,14 @@ var version string
 // Version returns the semantic version (see http://semver.org).
 func Version() string {
 	if version == "" {
-		verBuilder := bytes.NewBufferString(fmt.Sprintf("%d.%d.%d", major, minor, patch))
+		semver := fmt.Sprintf("%d.%d.%d", major, minor, patch)
+		verBuilder := bytes.NewBufferString(semver)
 		if tag != "" && tag != "-" {
 			updated := strings.TrimPrefix(tag, "-")
-			verBuilder.WriteRune('-')
-			verBuilder.WriteString(updated)
+			_, err := verBuilder.WriteString("-" + updated)
+			if err == nil {
+				verBuilder = bytes.NewBufferString(semver)
+			}
 		}
 		version = verBuilder.String()
 	}
