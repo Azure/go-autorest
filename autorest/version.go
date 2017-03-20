c1c5c282ccad3c838dbd9dd83a7d7fb4c7da4929
Cosmin Cojocar <cosmc@microsoft.com>
2017-03-20 19:24:01 +0100

    Remove the version check

--- autorest/version.go
+++ autorest/version.go
@@ -14,24 +14,22 @@ const (
 	tag   = ""
 )
 
+var once sync.Once
 var version string
 
 // Version returns the semantic version (see http://semver.org).
 func Version() string {
-	var once sync.Once
-	if version == "" {
-		once.Do(func() {
-			semver := fmt.Sprintf("%d.%d.%d", major, minor, patch)
-			verBuilder := bytes.NewBufferString(semver)
-			if tag != "" && tag != "-" {
-				updated := strings.TrimPrefix(tag, "-")
-				_, err := verBuilder.WriteString("-" + updated)
-				if err == nil {
-					verBuilder = bytes.NewBufferString(semver)
-				}
+	once.Do(func() {
+		semver := fmt.Sprintf("%d.%d.%d", major, minor, patch)
+		verBuilder := bytes.NewBufferString(semver)
+		if tag != "" && tag != "-" {
+			updated := strings.TrimPrefix(tag, "-")
+			_, err := verBuilder.WriteString("-" + updated)
+			if err == nil {
+				verBuilder = bytes.NewBufferString(semver)
 			}
-			version = verBuilder.String()
-		})
-	}
+		}
+		version = verBuilder.String()
+	})
 	return version
 }
