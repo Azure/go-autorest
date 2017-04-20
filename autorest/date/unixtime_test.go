c4ec7c26f756f0f3f937a0f15d8211c8219eb9db
Martin Strobel <marstr@microsoft.com>
2017-04-20 12:36:34 -0700

    Responding to review feedback

--- autorest/date/unixtime_test.go
+++ autorest/date/unixtime_test.go
@@ -1,4 +1,4 @@
-package date_test
+package date
 
 import (
 	"bytes"
@@ -7,19 +7,17 @@ import (
 	"fmt"
 	"testing"
 	"time"
-
-	"github.com/Azure/go-autorest/autorest/date"
 )
 
 func ExampleUnixTime_MarshalJSON() {
-	epoch := date.UnixTime(date.UnixEpoch())
+	epoch := UnixTime(UnixEpoch())
 	text, _ := json.Marshal(epoch)
 	fmt.Print(string(text))
 	// Output: 0
 }
 
 func ExampleUnixTime_UnmarshalSON() {
-	var myTime date.UnixTime
+	var myTime UnixTime
 	json.Unmarshal([]byte("1.3e2"), &myTime)
 	fmt.Printf("%v", time.Time(myTime))
 	// Output: 1970-01-01 00:02:10 +0000 UTC
@@ -27,22 +25,34 @@ func ExampleUnixTime_UnmarshalSON() {
 
 func TestUnixTime_MarshalJSON(t *testing.T) {
 	testCases := []time.Time{
-		date.UnixEpoch().Add(-1 * time.Second),                   // One second befote the Unix Epoch
+		UnixEpoch().Add(-1 * time.Second),                        // One second befote the Unix Epoch
 		time.Date(2017, time.April, 14, 20, 27, 47, 0, time.UTC), // The time this test was written
 	}
 
 	for _, tc := range testCases {
 		t.Run("", func(subT *testing.T) {
-			target := date.UnixTime(tc)
+			var actual, expected float64
+			var marshaled []byte
 
-			expected := string([]byte(fmt.Sprintf("%d", tc.Unix())))
-			if actual, err := json.Marshal(target); err != nil {
-				subT.Error(err)
-			} else if expected != string(actual) {
-				subT.Logf("got: \t%s\nwant:\t%s", string(actual), expected)
-				subT.Fail()
+			target := UnixTime(tc)
+			expected = target.Duration().Seconds()
+
+			if temp, err := json.Marshal(target); err == nil {
+				marshaled = temp
 			} else {
-				subT.Logf("passed with value: %s", string(actual))
+				subT.Error(err)
+				return
+			}
+
+			dec := json.NewDecoder(bytes.NewReader(marshaled))
+			if err := dec.Decode(&actual); err != nil {
+				subT.Error(err)
+				return
+			}
+
+			if actual != expected {
+				subT.Logf("\ngot :\t%g\nwant:\t%g", actual, expected)
+				subT.Fail()
 			}
 		})
 	}
@@ -53,21 +63,21 @@ func TestUnixTime_UnmarshalJSON(t *testing.T) {
 		text     string
 		expected time.Time
 	}{
-		{"1", date.UnixEpoch().Add(time.Second)},
-		{"0", date.UnixEpoch()},
+		{"1", UnixEpoch().Add(time.Second)},
+		{"0", UnixEpoch()},
 		{"1492203742", time.Date(2017, time.April, 14, 21, 02, 22, 0, time.UTC)}, // The time this test was written
 		{"-1", time.Date(1969, time.December, 31, 23, 59, 59, 0, time.UTC)},
-		{"1.5", date.UnixEpoch().Add(1500 * time.Millisecond)},
-		{"0e1", date.UnixEpoch()}, // See http://json.org for 'number' format definition.
-		{"1.3e+2", date.UnixEpoch().Add(130 * time.Second)},
-		{"1.6E-10", date.UnixEpoch()}, // This is so small, it should get truncated into the UnixEpoch
-		{"2E-6", date.UnixEpoch().Add(2 * time.Microsecond)},
-		{"1.289345e9", date.UnixEpoch().Add(1289345000 * time.Second)},
+		{"1.5", UnixEpoch().Add(1500 * time.Millisecond)},
+		{"0e1", UnixEpoch()}, // See http://json.org for 'number' format definition.
+		{"1.3e+2", UnixEpoch().Add(130 * time.Second)},
+		{"1.6E-10", UnixEpoch()}, // This is so small, it should get truncated into the UnixEpoch
+		{"2E-6", UnixEpoch().Add(2 * time.Microsecond)},
+		{"1.289345e9", UnixEpoch().Add(1289345000 * time.Second)},
 	}
 
 	for _, tc := range testCases {
 		t.Run(tc.text, func(subT *testing.T) {
-			var rehydrated date.UnixTime
+			var rehydrated UnixTime
 			if err := json.Unmarshal([]byte(tc.text), &rehydrated); err != nil {
 				subT.Error(err)
 				return
@@ -76,8 +86,6 @@ func TestUnixTime_UnmarshalJSON(t *testing.T) {
 			if time.Time(rehydrated) != tc.expected {
 				subT.Logf("\ngot: \t%v\nwant:\t%v\ndiff:\t%v", time.Time(rehydrated), tc.expected, time.Time(rehydrated).Sub(tc.expected))
 				subT.Fail()
-			} else {
-				subT.Logf("rehydrated matched expected '%v'", tc.expected)
 			}
 		})
 	}
@@ -85,30 +93,29 @@ func TestUnixTime_UnmarshalJSON(t *testing.T) {
 
 func TestUnixTime_JSONRoundTrip(t *testing.T) {
 	testCases := []time.Time{
-		date.UnixEpoch(),
+		UnixEpoch(),
 		time.Date(2005, time.November, 5, 0, 0, 0, 0, time.UTC), // The day V for Vendetta (film) was released.
-		date.UnixEpoch().Add(-6 * time.Second),
-		date.UnixEpoch().Add(800 & time.Hour),
+		UnixEpoch().Add(-6 * time.Second),
+		UnixEpoch().Add(800 & time.Hour),
 	}
 
 	for _, tc := range testCases {
 		t.Run(tc.String(), func(subT *testing.T) {
-			subject := date.UnixTime(tc)
+			subject := UnixTime(tc)
 			var marshaled []byte
 			if temp, err := json.Marshal(subject); err == nil {
 				marshaled = temp
 			} else {
-				t.Error(err)
+				subT.Error(err)
 				return
 			}
 
-			var unmarshaled date.UnixTime
+			var unmarshaled UnixTime
 			if err := json.Unmarshal(marshaled, &unmarshaled); err != nil {
-				t.Error(err)
-				return
+				subT.Error(err)
 			} else if time.Time(subject) != time.Time(unmarshaled) {
-				t.Logf("round trip failed for: %v", time.Time(subject))
-				t.Fail()
+				subT.Logf("round trip failed for: %v", time.Time(subject))
+				subT.Fail()
 			}
 		})
 	}
@@ -119,16 +126,16 @@ func TestUnixTime_MarshalBinary(t *testing.T) {
 		expected int64
 		subject  time.Time
 	}{
-		{0, date.UnixEpoch()},
-		{-15 * int64(time.Second), date.UnixEpoch().Add(-15 * time.Second)},
-		{54, date.UnixEpoch().Add(54 * time.Nanosecond)},
+		{0, UnixEpoch()},
+		{-15 * int64(time.Second), UnixEpoch().Add(-15 * time.Second)},
+		{54, UnixEpoch().Add(54 * time.Nanosecond)},
 	}
 
 	for _, tc := range testCases {
 		t.Run("", func(subT *testing.T) {
 			var marshaled []byte
 
-			if temp, err := date.UnixTime(tc.subject).MarshalBinary(); err == nil {
+			if temp, err := UnixTime(tc.subject).MarshalBinary(); err == nil {
 				marshaled = temp
 			} else {
 				subT.Error(err)
@@ -151,15 +158,15 @@ func TestUnixTime_MarshalBinary(t *testing.T) {
 
 func TestUnixTime_BinaryRoundTrip(t *testing.T) {
 	testCases := []time.Time{
-		date.UnixEpoch(),
-		date.UnixEpoch().Add(800 * time.Minute),
-		date.UnixEpoch().Add(7 * time.Hour),
-		date.UnixEpoch().Add(-1 * time.Nanosecond),
+		UnixEpoch(),
+		UnixEpoch().Add(800 * time.Minute),
+		UnixEpoch().Add(7 * time.Hour),
+		UnixEpoch().Add(-1 * time.Nanosecond),
 	}
 
 	for _, tc := range testCases {
 		t.Run(tc.String(), func(subT *testing.T) {
-			original := date.UnixTime(tc)
+			original := UnixTime(tc)
 			var marshaled []byte
 
 			if temp, err := original.MarshalBinary(); err == nil {
@@ -169,7 +176,7 @@ func TestUnixTime_BinaryRoundTrip(t *testing.T) {
 				return
 			}
 
-			var traveled date.UnixTime
+			var traveled UnixTime
 			if err := traveled.UnmarshalBinary(marshaled); err != nil {
 				subT.Error(err)
 				return
@@ -185,10 +192,10 @@ func TestUnixTime_BinaryRoundTrip(t *testing.T) {
 
 func TestUnixTime_MarshalText(t *testing.T) {
 	testCases := []time.Time{
-		date.UnixEpoch(),
-		date.UnixEpoch().Add(45 * time.Second),
-		date.UnixEpoch().Add(time.Nanosecond),
-		date.UnixEpoch().Add(-100000 * time.Second),
+		UnixEpoch(),
+		UnixEpoch().Add(45 * time.Second),
+		UnixEpoch().Add(time.Nanosecond),
+		UnixEpoch().Add(-100000 * time.Second),
 	}
 
 	for _, tc := range testCases {
@@ -196,7 +203,7 @@ func TestUnixTime_MarshalText(t *testing.T) {
 		t.Run("", func(subT *testing.T) {
 			var marshaled []byte
 
-			if temp, err := date.UnixTime(tc).MarshalText(); err == nil {
+			if temp, err := UnixTime(tc).MarshalText(); err == nil {
 				marshaled = temp
 			} else {
 				subT.Error(err)
@@ -213,15 +220,15 @@ func TestUnixTime_MarshalText(t *testing.T) {
 
 func TestUnixTime_TextRoundTrip(t *testing.T) {
 	testCases := []time.Time{
-		date.UnixEpoch(),
-		date.UnixEpoch().Add(-1 * time.Nanosecond),
-		date.UnixEpoch().Add(1 * time.Nanosecond),
+		UnixEpoch(),
+		UnixEpoch().Add(-1 * time.Nanosecond),
+		UnixEpoch().Add(1 * time.Nanosecond),
 		time.Date(2017, time.April, 17, 21, 00, 00, 00, time.UTC),
 	}
 
 	for _, tc := range testCases {
 		t.Run(tc.String(), func(subT *testing.T) {
-			unixTC := date.UnixTime(tc)
+			unixTC := UnixTime(tc)
 
 			var marshaled []byte
 
@@ -232,7 +239,7 @@ func TestUnixTime_TextRoundTrip(t *testing.T) {
 				return
 			}
 
-			var unmarshaled date.UnixTime
+			var unmarshaled UnixTime
 			if err := unmarshaled.UnmarshalText(marshaled); err != nil {
 				subT.Error(err)
 				return
