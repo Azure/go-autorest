c6303282deae424ca76cc88ddb951a77441bc78f
Joel Hendrix <jhendrix@microsoft.com>
2017-04-27 13:31:34 -0700

    Remove the adal example.
    It breaks travis and doesn't really belong here anyways.  Will move it to
    the examples repo.

--- autorest/adal/example-msi-app/test.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# RUN THIS ON THE REMOTE MACHINE WITH MSI EXTENSION ACTIVE
-
-wget 'https://colemickmsitest0.blob.core.windows.net/testapp/example-msi-app'
-chmod +x example-msi-app
-./example-msi-app
