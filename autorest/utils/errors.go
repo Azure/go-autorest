00d6ca44ee3a003f38a32e01023702ca6ade27c1
mcardosos <mcardoso@microsoft.com>
2017-07-14 14:21:37 -0700

    Panic instead of exit

--- autorest/utils/errors.go
+++ autorest/utils/errors.go
@@ -2,13 +2,11 @@ package utils
 
 import (
 	"fmt"
-	"os"
 )
 
-// OnErrorFail prints a failure message and exits the program if err is not nil.
-func OnErrorFail(err error, message string) {
+// OnErrorPanic prints a failure message and exits the program if err is not nil.
+func OnErrorPanic(err error, message string) {
 	if err != nil {
-		fmt.Printf("%s: %s", message, err)
-		os.Exit(1)
+		panic(fmt.Sprintf("%s: %s", message, err))
 	}
 }
