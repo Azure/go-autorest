b2a86f86d5e76bd0d10b7d70b8eb159083a58cb3
mcardosos <mcardoso@microsoft.com>
2017-08-31 11:17:01 -0700

    Nit

--- autorest/utils/errors.go
+++ autorest/utils/errors.go
@@ -7,6 +7,6 @@ import (
 // OnErrorPanic prints a failure message and exits the program if err is not nil.
 func OnErrorPanic(err error, message string) {
 	if err != nil {
-		panic(fmt.Sprintf("%s: %s", message, err))
+		panic(fmt.Sprintf("%s: %v", message, err))
 	}
 }
