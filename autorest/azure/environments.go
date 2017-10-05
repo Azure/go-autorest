f256c76c5f7e9288906c11247b29b3eee01289eb
Radhika Gupta <radhikgu@microsoft.com>
2017-10-05 15:58:58 -0700

    Updating to add AzureStack cloud. Since Azure Stack is a variable endpoint deployment, we are adding ability to bring your own endpoint.

--- autorest/azure/environments.go
+++ autorest/azure/environments.go
@@ -1,7 +1,10 @@
 package azure
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"path"
 	"strings"
 )
 
@@ -10,6 +13,7 @@ var environments = map[string]Environment{
 	"AZUREGERMANCLOUD":       GermanCloud,
 	"AZUREPUBLICCLOUD":       PublicCloud,
 	"AZUREUSGOVERNMENTCLOUD": USGovernmentCloud,
+	"AZURESTACKCLOUD":        AzureStackCloud,
 }
 
 // Environment represents a set of endpoints for each of Azure's Clouds.
@@ -117,6 +121,11 @@ var (
 		ResourceManagerVMDNSSuffix:   "cloudapp.microsoftazure.de",
 		ContainerRegistryDNSSuffix:   "azurecr.io",
 	}
+
+	//AzurestackCloud is the Azure enviornment running in customer datacenter
+	AzurestackCloud = Environment{
+		Name: "AzureStackCloud",
+	}
 )
 
 // EnvironmentFromName returns an Environment based on the common name specified
@@ -125,6 +134,20 @@ func EnvironmentFromName(name string) (Environment, error) {
 	env, ok := environments[name]
 	if !ok {
 		return env, fmt.Errorf("autorest/azure: There is no cloud environment matching the name %q", name)
+	}
+
+	if strings.EqualFold(name, "AZURESTACKCLOUD") == true {
+
+		println("autorest/azure: Reading Azure Stack Cloud config from directory", path.Join("etc", "kubernetes", "azurestackcloud.json"))
+		fbytes, err := ioutil.ReadFile(path.Join("/etc", "kubernetes", "azurestackcloud.json"))
+		if err != nil {
+			return env, fmt.Errorf("autorest/azure: Error opening Azure Stack Cloud config file - azurestackcloud.json %q", err.Error())
+		}
+
+		err = json.Unmarshal(fbytes, &env)
+		if err != nil {
+			return env, fmt.Errorf("autorest/azure: Error parsing for Azure Stack Cloud config file %q", err.Error())
+		}
 	}
 	return env, nil
 }
