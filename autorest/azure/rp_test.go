b0a8e231f3ae02999f4f8d13ed6c6675bce76565
mcardosos <mcardoso@microsoft.com>
2017-10-10 19:08:14 -0700

    More complete test

--- autorest/azure/rp_test.go
+++ autorest/azure/rp_test.go
@@ -3,21 +3,42 @@ package azure
 import (
 	"net/http"
 	"testing"
-	"time"
 
 	"github.com/Azure/go-autorest/autorest"
-
 	"github.com/Azure/go-autorest/autorest/mocks"
 )
 
 func TestRegisterResourceProvider(t *testing.T) {
 	client := mocks.NewSender()
-	client.AppendResponse(mocks.NewResponseWithStatus("MissingSubscriptionRegistration", http.StatusTooManyRequests))
+	client.AppendResponse(mocks.NewResponseWithBodyAndStatus(mocks.NewBody(`{
+	"error":{
+		"code":"MissingSubscriptionRegistration",
+		"message":"The subscription registration is in 'Unregistered' state. The subscription must be registered to use namespace 'Microsoft.EventGrid'. See https://aka.ms/rps-not-found for how to register subscriptions.",
+		"details":[
+			{
+				"code":"MissingSubscriptionRegistration",
+				"target":"Microsoft.EventGrid",
+				"message":"The subscription registration is in 'Unregistered' state. The subscription must be registered to use namespace 'Microsoft.EventGrid'. See https://aka.ms/rps-not-found for how to register subscriptions."
+			}
+		]
+	}
+}
+`), http.StatusConflict, "MissingSubscriptionRegistration"))
+
+	client.AppendResponse(mocks.NewResponseWithBodyAndStatus(mocks.NewBody(`{
+	"registrationState": "Registering"
+}
+`), http.StatusOK, "200 OK"))
+
+	client.AppendResponse(mocks.NewResponseWithBodyAndStatus(mocks.NewBody(`{
+	"registrationState": "Registered"
+}
+`), http.StatusOK, "200 OK"))
+
 	client.AppendResponse(mocks.NewResponseWithStatus("200 OK", http.StatusOK))
 
-	r, err := autorest.SendWithSender(client, mocks.NewRequest(),
+	r, err := autorest.SendWithSender(client, mocks.NewRequestForURL("https://lol/subscriptions/rofl"),
 		RegisterResourceProvider(),
-		autorest.DoRetryForStatusCodes(5, time.Duration(2*time.Second), autorest.StatusCodesForRetry...),
 	)
 	if err != nil {
 		t.Fatalf("got error: %v", err)
