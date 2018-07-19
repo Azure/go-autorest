54c7d523e6436f273a611ca9c4e6be0531fa2890
Joel Hendrix <jhendrix@microsoft.com>
2018-07-19 14:00:30 -0700

    Include user-agent info in token requests
    Add the standard go-autorest user-agent string to token requests.  This
    required moving version info to its own package to avoid a circular
    dependency.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,15 @@
 # CHANGELOG
 
+## v10.14.0
+
+### New Features
+
+- Added package version that contains version constants and user-agent data.
+
+### Bug Fixes
+
+- Add the user-agent to token requests.
+
 ## v10.13.0
 
 - Added support for additionalInfo in ServiceError type.
--- version/version.go
+++ version/version.go
@@ -1,6 +1,4 @@
-package autorest
-
-import "github.com/Azure/go-autorest/version"
+package version
 
 // Copyright 2017 Microsoft Corporation
 //
@@ -16,7 +14,21 @@ import "github.com/Azure/go-autorest/version"
 //  See the License for the specific language governing permissions and
 //  limitations under the License.
 
-// Version returns the semantic version (see http://semver.org).
-func Version() string {
-	return version.Number
-}
+import (
+	"fmt"
+	"runtime"
+)
+
+// Number contains the semantic version of this SDK.
+const Number = "v10.14.0"
+
+var (
+	// UserAgent builds a string containing the Go version, system archityecture and OS,
+	// and the go-autorest version.
+	UserAgent = fmt.Sprintf("Go/%s (%s-%s) go-autorest/%s",
+		runtime.Version(),
+		runtime.GOARCH,
+		runtime.GOOS,
+		Number,
+	)
+)
