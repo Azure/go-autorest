ad5c50337056320b53492bf8d2aec21ee7b9bccc
MartinForReal <fanshangxiang@gmail.com>
2022-07-19 19:04:22 +0800

    set MaxIdleConnsPerHost to 100 to reuse connections when there is a large volume of requests

--- autorest/sender.go
+++ autorest/sender.go
@@ -139,6 +139,7 @@ func sender(renengotiation tls.RenegotiationSupport) Sender {
 			}).DialContext,
 			ForceAttemptHTTP2:     true,
 			MaxIdleConns:          100,
+			MaxIdleConnsPerHost:   100,
 			IdleConnTimeout:       90 * time.Second,
 			TLSHandshakeTimeout:   10 * time.Second,
 			ExpectContinueTimeout: 1 * time.Second,
